c483fec71cad7f9bdd0966856453254444588901
Aleksandr <fairlybet@gmail.com>
2021-12-09 01:22:46 +0200

    several changes

--- Assets/Scripts/Game/Card.cs
+++ Assets/Scripts/Game/Card.cs
@@ -1,35 +1,30 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class Card : MonoBehaviour
 {
-   
-   [SerializeField] private GameObject elementPrefab;
-   [SerializeField] private Spawner fieldSpawner;
-   [SerializeField] private Spawner elementSpawner;
+    [SerializeField] private GameObject elementPrefab;
+    [SerializeField] private GameObject combinationPrefab;
+    [SerializeField] private Spawner spawner;
+    private bool inField = false;
 
-   private bool inField = false;
-   private void OnEnable()
-   {
-      SpawnElement();
-      SpawnField();
-   }
+    private void OnEnable()
+    {
+        SpawnElement();
+        SpawnField();
+    }
 
-   public void SpawnElement()
-   {
-      //elementSpawner.SpawnPrefab(elementPrefab);
-      elementSpawner.SpawnRandomPrefab();
-   }
-   public void SpawnField()
-   {
-      if(!inField)fieldSpawner.SpawnRandomPrefab();
-   }
+    public void SpawnElement()
+    {
+        spawner.SpawnPrefab(elementPrefab);
+    }
+    
+    public void SpawnField()
+    {
+        if (!inField) spawner.SpawnPrefab(combinationPrefab);
+    }
 
-   private void OnTriggerStay(Collider other)
-   {
-      inField = other.gameObject.GetComponent<Combination>() ? true : false;
-   }
+    private void OnTriggerStay(Collider other)
+    {
+        inField = other.gameObject.GetComponent<Combination>() ? true : false;
+    }
 }
