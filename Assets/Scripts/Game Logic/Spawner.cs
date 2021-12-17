cc1f1d112c8c739039f4ae30f284ddc2df7493ae
Aleksandr <fairlybet@gmail.com>
2021-12-17 17:18:40 +0200

    dab dab

--- Assets/Scripts/Game Logic/Spawner.cs
+++ Assets/Scripts/Game Logic/Spawner.cs
@@ -69,6 +69,7 @@ public class Spawner : MonoBehaviour
         spawnPrefabEvent?.Invoke();
         var obj = Object.Instantiate(prefab);
         obj.transform.position = transform.position;
+        obj.transform.parent = spawnParent;
 
         // foreach (GameObject obj in spawnerData.Prefabs)
         // {
